74c544416b55cd4146b5f69839f3cb086f5ec485
mikigo <1964191531@qq.com>
2025-07-02 14:11:51 +0800

    update

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-<h1 style="text-align: center"><span style="color:#0095ff">mikigo</span>'s <span style="color:#0095ff">docs</span></h1>
+# mikigo's docs
 
 这是我的个人文档仓库，主要用于记录我的学习、工作和生活。
 
--- theme/index.tsx
+++ theme/index.tsx
@@ -2,9 +2,22 @@ import './index.css';
 
 import Theme from 'rspress/theme';
 
+const myStyle = {
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+}
+
+const Layout = () => (
+  <Theme.Layout
+    afterFeatures={<div align="center" style={myStyle}>本站总访问量:<img src="https://w.saobby.com/w/dbbsuky7" alt="访问统计"/></div>}
+  />
+);
+
 
 export default {
   ...Theme,
+  Layout,
 };
 
 export * from 'rspress/theme';
